ed9f5e97d1f0c623bec206553cd30d1e8c1dfbb6
Yorzoda Mahmadsaid <48682215+YorzodaMahmadsaid@users.noreply.github.com>
2020-09-05 19:06:28 +0500

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 let names: Array<string> = ['Said', 'Navruz', 'Jonon'];
 console.log(names);
 
-let arr: string[] = ['Man', 'Mallm', 'Lalal'];
-console.log(arr);
+let arrayOfNames: string[] = ['Said', 'Navruz', 'Firuz'];
+console.log(arrayOfNames);
 
